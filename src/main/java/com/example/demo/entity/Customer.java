0ecda346503a91437080958e29af89aabaac3906
m44rx <73678748+m44rx@users.noreply.github.com>
2021-11-30 16:30:16 -0500

    complete entities

--- src/main/java/com/example/demo/entity/Customer.java
+++ src/main/java/com/example/demo/entity/Customer.java
@@ -1,5 +1,59 @@
 package com.example.demo.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tbl_customer")
 public class Customer {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer idCustomer;
+	private String nameCustomer;
+	
+	
+	@ManyToMany(mappedBy = "customers", cascade = {CascadeType.MERGE,CascadeType.PERSIST,CascadeType.DETACH})
+	List<Employee> employees = new ArrayList<>();
+
+
+	public Integer getIdCustomer() {
+		return idCustomer;
+	}
+
+
+	public void setIdCustomer(Integer idCustomer) {
+		this.idCustomer = idCustomer;
+	}
+
+
+	public String getNameCustomer() {
+		return nameCustomer;
+	}
+
+
+	public void setNameCustomer(String nameCustomer) {
+		this.nameCustomer = nameCustomer;
+	}
+
+
+	public List<Employee> getEmployees() {
+		return employees;
+	}
+
+
+	public void setEmployees(List<Employee> employees) {
+		this.employees = employees;
+	}
+	
+	
+	
 }
--- src/main/java/com/example/demo/entity/Employee.java
+++ src/main/java/com/example/demo/entity/Employee.java
@@ -1,8 +1,60 @@
 package com.example.demo.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tbl_employee")
 public class Employee {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer idEmployee;
 	private String name;
-	private String lastname;
+
+	@ManyToMany(cascade = { CascadeType.MERGE, CascadeType.DETACH, CascadeType.PERSIST })
+	@JoinTable(name = "cust_emplo", 
+	joinColumns = @JoinColumn(name = "fk_employee", referencedColumnName = "idEmployee"),
+	inverseJoinColumns = @JoinColumn(name = "fk_customer", referencedColumnName = "idCustomer")
+	)
+	List<Customer> customers = new ArrayList<>();
+
+	public Integer getIdEmployee() {
+		return idEmployee;
+	}
+
+	public void setIdEmployee(Integer idEmployee) {
+		this.idEmployee = idEmployee;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public List<Customer> getCustomers() {
+		return customers;
+	}
+
+	public void setCustomers(List<Customer> customers) {
+		this.customers = customers;
+	}
+	
+	
+	
+	
+
 }
